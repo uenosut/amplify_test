c56193074fa5db3f083f02a28a8619301a84489e
ueno syouta <uenon@ueno-no-MacBook-Pro.local>
2023-02-24 11:51:13 +0900

    added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp3": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyapp3a1ebb610": {
       "AppClientID": "string",
